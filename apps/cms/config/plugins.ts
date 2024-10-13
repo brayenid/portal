b6e3afd998501b1a7d824c2b77115b690fe91333
Anthonius Munthi <me@itstoni.com>
2024-10-13 18:36:35 +0800

    feat(cms): configured cms node mailer, ref #87 :sparkles:

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -20,4 +20,22 @@ export default ({ env }) => ({
       };
     })(),
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp-relay.gmail.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "hello@pkrbt.id",
+        defaultReplyTo: "hello@pkrbt.id",
+      },
+    },
+  },
 });

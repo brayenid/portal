89e281578741a734a7cdc41b5bf2c40e02033d91
Anthonius Munthi <me@itstoni.com>
2024-10-14 12:46:01 +0800

    feat(cms): configured sentry for web app, ref #93 :rocket:

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7c2cc72d6ed45a27f3f059437ee4b36f@o4508010236674048.ingest.us.sentry.io/4508102001098752",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});

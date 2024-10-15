66a3b32b0e4465c41453ffce6e4cac685abd956a
Anthonius Munthi <me@itstoni.com>
2024-10-15 16:24:48 +0800

    feat(cms): configured sentry for cms app :hammer:

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,4 +1,11 @@
 export default ({ env }) => ({
+  sentry: {
+    enabled: true,
+    config: {
+      dsn: env("SENTRY_DSN"),
+      sendMetadata: true,
+    },
+  },
   upload: {
     config: (() => {
       if ("production" != process.env.NODE_ENV) {
